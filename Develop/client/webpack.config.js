7ef4e88313a5cc4a63d2f81e40c73b0aba7d804b
lbako801 <lbako98@gmail.com>
2023-02-21 22:23:06 -0700

    added .gitignore, working on bug during npm run build. Need to check webpack config image path

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -34,18 +34,9 @@ module.exports = () => {
         crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
         icons: [
           {
-            src: path.resolve('src/assets/icon.png'),
+            src: path.resolve('src/images/icon.png'),
             sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-          },
-          {
-            src: path.resolve('src/assets/large-icon.png'),
-            size: '1024x1024' // you can also use the specifications pattern
-          },
-          {
-            src: path.resolve('src/assets/maskable-icon.png'),
-            size: '1024x1024',
-            purpose: 'maskable'
-          }]
+          },]
       })
     ],
 
