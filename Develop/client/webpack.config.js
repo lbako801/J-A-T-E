b76bd3bf8908c8016f900451f9ba11269049275e
lbako801 <lbako98@gmail.com>
2023-02-23 21:45:01 -0700

    changed service workers rules to fix a little bug!

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -35,7 +35,8 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join('assets', 'icons'),
           },]
       })
     ],
